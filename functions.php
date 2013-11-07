bb29dd081b51b174c3b98f7e2aff451db5f9b9ab
Thomas Ongeri <tongeri@juilliard.edu>
2013-11-07 17:46:20 -0500

    new files

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+//Some simple code for our widget-enabled sidebar
+if ( function_exists('register_sidebar') )
+    register_sidebar();
+
+//Add support for WordPress 3.0's custom menus
+add_action( 'init', 'register_my_menu' );
+
+//Register area for custom menu
+function register_my_menu() {
+	register_nav_menu( 'primary-menu', __( 'Primary Menu' ) );
+}
+
+//Code for custom background support
+add_custom_background();
+
+//Enable post and comments RSS feed links to head
+add_theme_support( 'automatic-feed-links' );
+
+// Enable post thumbnails
+add_theme_support('post-thumbnails');
+set_post_thumbnail_size(520, 250, true);
+?>
