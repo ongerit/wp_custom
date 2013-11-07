a70f179f5548a9abc2d7ce01768cbf56ddd12604
Thomas Ongeri <ongerit@gmail.com>
2013-11-07 17:40:59 -0500

    added files

---
+++ firstcustom/footer.php
@@ -0,0 +1,12 @@
+<div id="footer">
+	<p>
+	<strong>Copyright 2011 <?php bloginfo('name'); ?> | All Rights Reserved.</strong> </a> 
+	Designed by <a href="http://thomasongeri.com">Thomas Ongeri</a>
+	</p>
+	<p><a href="<?php bloginfo('rss2_url'); ?>">Latest Stories RSS</a> | <a href="<?php comments_rss_link('comment feed'); ?>">Comments RSS</a></p>
+	
+	</div>
+</div>
+
+</body>
+</html>
